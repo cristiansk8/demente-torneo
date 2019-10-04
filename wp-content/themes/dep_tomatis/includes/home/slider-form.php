f99c777edb2bb0137bda4558a5c2d0eb2c57272d
unknown <cdpsk8@gmail.com>
2019-10-04 16:51:30 -0500

    primer banner y formulario

--- wp-content/themes/dep_tomatis/includes/home/slider-form.php
+++ wp-content/themes/dep_tomatis/includes/home/slider-form.php
@@ -1,141 +1,26 @@
 <section id="slider-form">
-  <div class="slider-form">
+  <div class="banner-principal">
 
-    <div class="container-fluid" style="background-image:url(https://edificiouno83.com/wp-content/uploads/2019/09/banner-uno83.png); height:auto;background-size: cover;">
+    <div class="container-fluid" style="background-image:url(https://dementetorneo.site/wp-content/uploads/2019/10/EI_276854_20140801_JF1_0503.jpg); height:auto;background-size: cover;">
       <div class="row text-left logo">
           <img src="https://depura-creatividad.com/landinguno83/wp-content/uploads/2019/08/logo-uno83.png" alt="" width="300px" height="auto">
       </div>
       <div class="row">
         <div class="col-md-8 text-slider text-center">
           <div class="text-big">
-            <h1>APARTAMENTOS <span>DESDE</span> $152'000,000</h1>
+            <h1>REGISTRATE A NUESTRA <span>RIFA</span> Y PARTICIPA POR UNOS TENIS</h1>
           </div>
-          <div class="oferta"> <span>precios de lanzamiento</span> </div>
+          <div class="oferta"> <span>VER TERMINOS Y CONDICIONES</span> </div>
         </div>
         <div class="col-md-3 form-gral">
           <div class=" formulario-contacto">
             <h3>
-                ¿QUIERES SABER<br> MÁS SOBRE <br><span>TU NUEVO HOGAR</span> ?
+                REGISTRATÉ<br><span>ES GRATIS!</span>
            </h3>
           </div>
 
          <div class="">
-           <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
-<script>
-  hbspt.forms.create({
-	portalId: "6326850",
-	formId: "fcb014e8-6db9-4033-b835-224c680bff5f"
-});
-</script>
-         </div>
-
-        </div>
-        <div class="col-md-1">
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container-fluid" style="background-image:url(http://depura-creatividad.com/landinguno83/wp-content/uploads/2019/08/Landing-183-13ago2019-cristian-03.jpg); height:auto;background-size: cover;">
-      <div class="row text-left logo">
-          <img src="https://depura-creatividad.com/landinguno83/wp-content/uploads/2019/08/logo-uno83.png" alt="" width="300px" height="auto">
-      </div>
-      <div class="row">
-        <div class="col-md-8 text-slider">
-          <div class="text-big">
-            <h1>COMPRAR APTO EN <br><span>BOGOTÁ SÍ SE PUEDE</span></h1>
-          </div>
-
-<div class="oferta"> <span>precios de lanzamiento</span> </div>
-        </div>
-        <div class="col-md-3 form-gral">
-          <div class=" formulario-contacto">
-            <h3>
-                ¿QUIERES SABER<br> MÁS SOBRE <br><span>TU NUEVO HOGAR</span> ?
-           </h3>
-          </div>
-
-         <div class="">
-           <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
-<script>
-  hbspt.forms.create({
- portalId: "6326850",
- formId: "fcb014e8-6db9-4033-b835-224c680bff5f"
-});
-</script>
-         </div>
-
-        </div>
-        <div class="col-md-1">
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container-fluid" style="background-image:url(http://depura-creatividad.com/landinguno83/wp-content/uploads/2019/08/Landing-183-13ago2019-cristian-02.jpg); height:auto;background-size: cover;">
-      <div class="row text-left logo">
-          <img src="https://depura-creatividad.com/landinguno83/wp-content/uploads/2019/08/logo-uno83.png" alt="" width="300px" height="auto">
-      </div>
-      <div class="row">
-        <div class="col-md-8 text-slider ">
-          <div class="text-big text-rosa">
-            <h1> <span>TÚ PERSONALIZAS</span>  <br>TU APARTAMENTO</h1>
-          </div>
-
-            <div class="oferta"> <span>precios de lanzamiento</span> </div>
-        </div>
-
-        <div class="col-md-3 form-gral">
-          <div class=" formulario-contacto">
-            <h3>
-                ¿QUIERES SABER<br> MÁS SOBRE <br><span>TU NUEVO HOGAR</span> ?
-           </h3>
-          </div>
-
-         <div class="">
-           <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
-<script>
-  hbspt.forms.create({
-  portalId: "6326850",
-  formId: "fcb014e8-6db9-4033-b835-224c680bff5f"
-});
-</script>
-         </div>
-
-        </div>
-        <div class="col-md-1">
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container-fluid" style="background-image:url(http://depura-creatividad.com/landinguno83/wp-content/uploads/2019/08/Landing-183-13ago2019-cristian-03.jpg); height:auto;background-size: cover;">
-      <div class="row text-left logo">
-          <img src="https://depura-creatividad.com/landinguno83/wp-content/uploads/2019/08/logo-uno83.png" alt="" width="300px" height="auto">
-      </div>
-      <div class="row">
-        <div class="col-md-8 text-slider">
-          <div class="text-big">
-            <h1>TÚ LO ESCOGES<br><span>TÚ LO PERSONALIZAS</span></h1>
-          </div>
-
-<div class="oferta"> <span>precios de lanzamiento</span> </div>
-        </div>
-        <div class="col-md-3 form-gral">
-          <div class=" formulario-contacto">
-            <h3>
-                ¿QUIERES SABER<br> MÁS SOBRE <br><span>TU NUEVO HOGAR</span> ?
-           </h3>
-          </div>
-
-         <div class="">
-           <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
-<script>
-  hbspt.forms.create({
-  portalId: "6326850",
-  formId: "fcb014e8-6db9-4033-b835-224c680bff5f"
-});
-</script>
+            <?php echo do_shortcode('[contact-form-7 id="16" title="formulario"]'); ?>
          </div>
 
         </div>
